a32f7ad40916cd52adb431fc28b6ef08f9806d3a
Casper  Sejrup <sejrup112@gmail.com>
2024-05-29 17:50:34 +0200

    change redirect to origin window and save access token local storage.

--- packages/frontend/src/main.js
+++ packages/frontend/src/main.js
@@ -1,18 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// main.js or main.ts
+import { createApp } from 'vue';
+import App from './App.vue';
 import { createAuth0 } from '@auth0/auth0-vue';
-
 
 const app = createApp(App);
 
 app.use(
-    createAuth0({
-      domain: "pbsw.eu.auth0.com",
-      clientId: "B8Wl4W10gRdaldhiH03BDkfWl3EO9WAR",
-      authorizationParams: {
-        redirect_uri: "http://localhost:4040/callback"
-      }
-    })
-  );
+  createAuth0({
+    domain: "pbsw.eu.auth0.com",
+    clientId: "B8Wl4W10gRdaldhiH03BDkfWl3EO9WAR",
+    authorizationParams: {
+      redirect_uri: window.location.origin,
+    },
+    cacheLocation: 'localstorage', // This is not recommended for sensitive data
+  })
+);
 
 app.mount('#app');
