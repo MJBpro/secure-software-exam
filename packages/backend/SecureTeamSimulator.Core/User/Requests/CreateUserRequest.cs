e7dad12dd335616fd51dd152e9ebc1f5859bc09e
Patrick Jakobsen Jahn <55191533+PatrickJahn@users.noreply.github.com>
2024-06-11 14:11:15 +0200

    Removed unused code

--- packages/backend/SecureTeamSimulator.Core/User/Requests/CreateUserRequest.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace SecureTeamSimulator.Core.User.Requests;
-
-public class CreateUserRequest
-{
-    [Required] public string Name { get; set; } = null!;
-    [Required] public string Email { get; set; } = null!;
-}
